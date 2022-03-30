d9e77673722d592550c69d3c888f974546fbbf4a
Bhavesh Borisaniya <bhavesh.borisaniya@gmail.com>
2022-03-30 12:24:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
   <h1>Welcome to Web App Services</h1>
   <?php 
       echo "<h2>This is my 1st web application</h2>";
+      $x = 1;
+      while($x <= 10) {
+        echo "The number is: $x <br>";
+        $x++;
+      }
   ?>
 </body>
 </html>
